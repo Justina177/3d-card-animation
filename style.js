547f4f9d96cb4130a31a9f8b81923c72310e619d
justina177 <justinaenenche@yahoo.com>
2022-11-18 14:09:24 +0100

    Added Animation Event

---
+++ style.js
@@ -0,0 +1,11 @@
+//Moving Animation Event
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+
+//Moving Animation Event
+container.addEventListener("mousemove", (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 20;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 20;
+
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg`
+})
